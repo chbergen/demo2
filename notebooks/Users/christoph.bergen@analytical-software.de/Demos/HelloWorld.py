180c9399d1543b7ee54ee0a803c0926a0b9fe616
chbergen <39091196+chbergen@users.noreply.github.com>
2018-08-14 14:59:49 +0200

    Test for Demo

---
+++ notebooks/Users/christoph.bergen@analytical-software.de/Demos/HelloWorld.py
@@ -0,0 +1,9 @@
+# Databricks notebook source
+dbutils.widgets.text("input", "","")
+dbutils.widgets.get("input")
+y = getArgument("input")
+print "Param -\'input':"
+print y
+
+# COMMAND ----------
+
